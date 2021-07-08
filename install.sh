aca35156281a7c168c50ab03e35a733c200a3b5c
Felipe Trzaskowski <felipe.trzaskowski@gmail.com>
2021-07-08 02:02:22 -0300

    cleanup

--- install.sh
+++ install.sh
@@ -7,19 +7,17 @@ ln -fs ${ROOT}/shell/pam_environment ${HOME}/.pam_environment
 echo -n 'source ~/.bashrc' >${HOME}/.bash_profile
 ln -fs ${ROOT}/shell/bashrc ${HOME}/.bashrc
 
-ln -fs ${ROOT}/misc/Xdefaults ${HOME}/.Xdefaults
 ln -fs ${ROOT}/misc/gitconfig ${HOME}/.gitconfig
+ln -fs ${ROOT}/misc/clang-format ${HOME}/.clang-format
+ln -fs ${ROOT}/misc/prettierrc ${HOME}/.prettierrc
+ln -fs ${ROOT}/misc/pylintrc ${HOME}/.pylintrc
 
 # this is not a link because this file is altered with other stuff later
 if [ ! -f ${HOME}/.ssh/config ]; then
     mkdir -p ${HOME}/.ssh
-    cp ${ROOT}/misc/sshconfig > ${HOME}/.ssh/config
+    cp ${ROOT}/misc/sshconfig ${HOME}/.ssh/config
 fi
 
 CONFIG=${HOME}/.config
 mkdir -p ${CONFIG}
 ln -fs ${ROOT}/config/* ${CONFIG}
-
-mkdir -p ${HOME}/.local/share/icons
-ln -fs ${ROOT}/misc/icons/* ${HOME}/.local/share/icons
-ln -fs ${HOME}/.local/share/icons ${HOME}/.icons
